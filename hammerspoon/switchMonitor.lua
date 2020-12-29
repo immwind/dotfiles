eb11fce46657c1378de394f98735ae03f24315c8
immwind <shisan@outlook.com>
2020-12-29 23:06:56 +0800

    fix inputSource 默认变量值

--- hammerspoon/switchMonitor.lua
+++ hammerspoon/switchMonitor.lua
@@ -4,10 +4,10 @@
 -- ddcctl: https://github.com/kfix/ddcctl
 
 hs.hotkey.bind(hypers, 's', function()
-    local inputSource = 15
+    local inputSource = 27
 
     if hs.host.localizedName() == "Surface Pro" then
-        inputSource = 27
+        inputSource = 15
     end
 
     hs.execute("~/.ddcctl/ddcctl -d 1 -i " .. inputSource)
