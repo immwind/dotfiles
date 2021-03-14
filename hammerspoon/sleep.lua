81f60e9a922c209a5d91aea3577f680f90dda011
immwind <shisan@outlook.com>
2021-03-14 22:15:55 +0800

    Updated 仅在连接电池时断开 Wi-Fi 和蓝牙

--- hammerspoon/sleep.lua
+++ hammerspoon/sleep.lua
@@ -1,16 +1,18 @@
 -- -------------------------------------------------------------------------- --
--- 监测睡眠状态执行指定操作
+-- 监测睡眠状态并执行指定操作
 --
 -- 前置：brew install blueutil (on: 1, off: 0)
 
 local caffeinateWatcher = require("hs.caffeinate.watcher")
 
 cs = caffeinateWatcher.new(function(screenEvent)
-    if screenEvent == caffeinateWatcher.screensDidSleep then
-        hs.wifi.setPower(false)
-        hs.execute("/usr/local/bin/blueutil --power 0")
-    elseif screenEvent == caffeinateWatcher.screensDidWake then
-        hs.wifi.setPower(true)
-        hs.execute("/usr/local/bin/blueutil --power 1")
+    if hs.battery.powerSource() == "Battery Power" then
+        if screenEvent == caffeinateWatcher.screensDidSleep then
+            hs.wifi.setPower(false)
+            hs.execute("/usr/local/bin/blueutil --power 0")
+        elseif screenEvent == caffeinateWatcher.screensDidWake then
+            hs.wifi.setPower(true)
+            hs.execute("/usr/local/bin/blueutil --power 1")
+        end
     end
 end):start()
