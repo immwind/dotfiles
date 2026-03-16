2a89934d3e1229334a1ee1914eac5509058ee488
immwind <i@immwind.com>
2026-03-16 07:26:10 +0000

    refactor 移除 shellAliases (已在 fish.nix) 实现

--- home.nix
+++ home.nix
@@ -9,15 +9,10 @@
   home.username = "double_u";
   home.homeDirectory = "/home/double_u";
 
-  home.stateVersion = "25.11"; # Home Manager 版本
-  home.packages = [];          # 安装 Nix 软件包
-  home.file = {};              # 需要管理的文件
-  home.sessionVariables = {};  # 配置 Shell 环境变量
-
-  # shell 别名
-  home.shellAliases = {
-    hms = "home-manager switch --flake ~/dotfiles#double_u@ubuntu";
-  };
+  home.stateVersion = "25.11";         # Home Manager 版本
+  home.packages = [];                  # 安装 Nix 软件包
+  home.file = {};                      # 需要管理的文件
+  home.sessionVariables = {};          # 配置 Shell 环境变量
 
   programs.home-manager.enable = true; # 让 Home Manager 管理自身
 }
