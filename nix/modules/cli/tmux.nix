9df2b85a035d7ef05972affc8b3677389faed6c1
immwind <i@immwind.com>
2026-03-16 18:04:43 +0000

    feat 支持在 tmux 中开关本地 prefix，在关闭时修改主题并把状态栏移到顶部

--- nix/modules/cli/tmux.nix
+++ nix/modules/cli/tmux.nix
@@ -25,6 +25,24 @@
       bind | split-window -h -c "#{pane_current_path}"  # 水平分割窗口
 
       bind f display-popup -E -w 70% -h 50% "fzf"       # 使用 fzf 搜索文件
+
+      # 禁用本地 prefix 后把状态栏移到顶部，并改为 snow 主题
+      bind -T root ` \
+        set prefix None \;\
+        set key-table off \;\
+        set status-position top \;\
+        set -g @tmux_power_theme 'snow' \;\
+        run-shell ${pkgs.tmuxPlugins.power-theme.rtp} \;\
+        refresh-client -S
+
+      # 恢复本地 prefix 和状态栏到底部，并改回 redwine 主题
+      bind -T off ` \
+        set -u prefix \;\
+        set -u key-table \;\
+        set -u status-position \;\
+        set -g @tmux_power_theme 'redwine' \;\
+        run-shell ${pkgs.tmuxPlugins.power-theme.rtp} \;\
+        refresh-client -S
   '';
   };
 }
