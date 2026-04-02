52daaf2e65518536cfb4ddea83ee2e7f98190167
immwind <i@immwind.com>
2026-04-02 07:15:30 +0000

    fix linux-server 禁用 GPU 集成

--- nix/hosts/linux-server.nix
+++ nix/hosts/linux-server.nix
@@ -1,3 +1,4 @@
 { ... }: {
-  targets.genericLinux.enable = true; # 非 NixOS 发行版的 Linux 需要配置
+  targets.genericLinux.enable = true;      # 非 NixOS 发行版的 Linux 需要配置
+  targets.genericLinux.gpu.enable = false; # 关闭非 NixOS GPU 集成
 }
