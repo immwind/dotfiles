c6f335bd7c639d7cb7f4e05452ec0ccb499cdfcc
immwind <i@immwind.com>
2026-04-05 09:03:10 +0000

    refactor modules/fish.nix 统一放到 shell 目录中

--- nix/modules/base.nix
+++ nix/modules/base.nix
@@ -3,12 +3,12 @@
 {
   imports = [
     ./cli/bat.nix
-    ./cli/fish.nix
     ./cli/fzf.nix
     ./cli/git.nix
     ./cli/neovim.nix
     ./cli/starship.nix
     ./cli/tmux.nix
     ./cli/uv.nix
+    ./shell/fish.nix
   ];
 }
